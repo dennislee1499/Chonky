dfdfc80acc563c1ec02cbd3446146413b9ca9e69
Dennis Lee <dennislee1499@gmail.com>
2023-09-14 22:54:25 -0400

    restoreSession method/ update index.js/ dynamic fetch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { restoreSession } from "./store/csrf";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+restoreSession().then(initializeApp);
+
+
+const initializeApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
