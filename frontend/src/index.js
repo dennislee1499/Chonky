abf2d199a97e81e53a43f62cedc7b89a2a96c9b1
Dennis Lee <dennislee1499@gmail.com>
2023-09-15 09:17:43 -0400

    restructured index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,27 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { restoreSession } from "./store/csrf";
+import { createUser, loginUser, logoutUser } from "./store/userReducer";
+import { Provider } from "react-redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import userReducer from './store/usersReducer';
 
 
 
-restoreSession().then(initializeApp);
+window.createUser = createUser;
+window.loginUser = loginUser;
+window.logoutUser = logoutUser;
 
- const rootReducer = combineReducers({
-   teas: teaReducer,
-   transactions: transactionReducer,
-   user: userReducer,
- });
+let currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+let initialState = {};
 
- let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
-    let initialState = {};
 
     if (currentUser) {
         initialState = {
-            users: {
+            user: {
             [currentUser.id]: currentUser
             }
         };
     };
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+
 
 const initializeApp = () => {
   ReactDOM.render(
@@ -35,3 +45,6 @@ const initializeApp = () => {
     document.getElementById("root")
   );
 };
+
+restoreSession().then(initializeApp);
+
