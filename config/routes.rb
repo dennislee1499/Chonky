ba1e3180a7a629154a589c780a1e258ea9bf87ea
Dennis Lee <dennislee1499@gmail.com>
2023-09-19 10:45:55 -0400

    Initial backend configs for render push

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ namespace :api, defaults: { format: :json } do
   resources :users, only: [:create]
   resource :session, only: [:create, :show, :destroy]
   end
+
+  get '*path', to: "static_pages#frontend_index"
 end
