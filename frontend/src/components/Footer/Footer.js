cd2a174d1d10865a72f4c4585e02951cf078a476
Dennis Lee <dennislee1499@gmail.com>
2023-09-26 23:03:49 -0400

    added github to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,10 +1,17 @@
+import github from "./github.png"
+
+
+
 export default function Footer() {
   return (
     <>
       <div className="footer-divider"></div>
-        <div className="footer-container">
-          <p>Copyright 2023, Chonky CEO: Dennis Lee</p>
-        </div>
+      <div className="footer-container">
+        <p>Copyright 2023, Chonky CEO: Dennis Lee</p>
+        <a href="https://github.com/dennislee1499" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="Github Icon" className="github-icon"></img>
+        </a>
+      </div>
     </>
   );
 }
