000582b71a38c90829f339ae68be691d72cc0e76
Dennis Lee <dennislee1499@gmail.com>
2023-10-28 18:12:57 -0400

    footer margin adjusted/ added linkedin icon

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import github from "./github.png"
+import linkedin from "./linkedin.png"
 
 
 
@@ -6,12 +7,17 @@ export default function Footer() {
   return (
     <>
       <div className="footer-divider"></div>
-      <div className="footer-container">
-        <p>Copyright 2023, Chonky CEO: Dennis Lee</p>
-        <a href="https://github.com/dennislee1499" target="_blank" rel="noopener noreferrer">
-          <img src={github} alt="Github Icon" className="github-icon"></img>
-        </a>
-      </div>
+        <div className="footer-container">
+          <p>Copyright 2023, Chonky CEO: Dennis Lee</p>
+          <div className="icon-container">
+            <a href="https://github.com/dennislee1499" target="_blank" rel="noopener noreferrer">
+              <img src={github} alt="Github Icon" className="github-icon"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/dennislee-/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedin} alt="Linkedin Icon" className="linkedin-icon"></img>
+            </a>
+          </div>
+        </div>
     </>
   );
 }
