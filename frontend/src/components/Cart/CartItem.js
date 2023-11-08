c36ee498b8f36d3eac3da4e9d29d8d95c4113e1d
Dennis Lee <dennislee1499@gmail.com>
2023-11-07 20:24:38 -0500

    display flavor on cart dropdown

--- frontend/src/components/Cart/CartItem.js
+++ frontend/src/components/Cart/CartItem.js
@@ -16,6 +16,7 @@ export default function CartItem({item}) {
         <div className="cart-item-info">
           <p className="cart-item-name">{product?.name}</p>{" "}
           <div className="cart-item-details">
+            <p className="item-flavor">Flavor: {item.flavor}</p>
             <p className="item-price">${parseFloat(item.price).toFixed(2)}</p>
             <p className="item-quantity">(Qty: {item.quantity})</p>
           </div>
